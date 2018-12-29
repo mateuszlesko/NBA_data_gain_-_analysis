f2aafc36b892ad078ece97451892361fbaac561e
Mateusz Leśko <31209474+mateuszlesko@users.noreply.github.com>
2018-12-29 14:48:49 +0100

    NBA scoreboard
    NBA scoreboard

---
+++ NBAScoreboard.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 28 21:04:00 2018
+
+@author: 4ever
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get('https://sports.yahoo.com/nba/scoreboard/')
+soup = BeautifulSoup(page.content,'html.parser')
+
+list(soup.children)
+
+[type(item) for item in list(soup.children)]
+
+teams = []
+score = []
+scoreboard = {}
+
+#print(soup.find_all('span',{'data-tst':'first-name'}))
+
+string = soup.find_all('span',{'data-tst':'first-name'})
+
+for s in string:
+    
+ val = s.get_text()
+ teams.append(val)
+
+string= soup.find_all('div',{'class':'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'})
+print(string[2])
+for s in string:
+    val = s.get_text()
+    score.append(val)
+
+for x in range(0,2):
+    teams.pop(0)
+    
+#print(teams)
+
+if len(score) == len(teams):
+    for x in range (0,len(teams),1):
+       print(teams[x],score[x],sep=':') 
