03d13cc36238c77bdfd3929255c1b16597770375
Mateusz Leśko <mateusz.lesko@op.pl>
2019-01-04 18:50:08 +0100

    reduce code
    reduce useless code, comments

--- NBAScoreboard.py
+++ NBAScoreboard.py
@@ -5,15 +5,7 @@ class Scoreboard(object):
     def __init__(self,name,score):
         self.name = name
         self.score = score
-    def HomeWon(s1,s2):
-        won = None
-        
-        if s1>s2:
-            won = True
-        else:
-            won = False
-        
-        return won
+    
         
 
 d = input('date format (yyyy-mm-dd): ')
@@ -27,11 +19,8 @@ list(soup.children)
 
 teams = []
 score = []
-scoreInt = [int(i) for i in score]
 scoreboard = []
 
-
-#print(soup.find_all('span',{'data-tst':'first-name'}))
 
 string = soup.find_all('span',{'data-tst':'first-name'})
 
@@ -46,30 +35,19 @@ for s in string:
     val = s.get_text()
     score.append(val)
 
-#for x in range(0,2):
-    #teams.pop(0)
     
 print(len(teams),len(score),sep=',')
 for x in range (0,len(teams),1):
-  #print(teams[x],score[x],sep=':') 
+  
   scoreboard.append(Scoreboard(teams[x],int(score[x])))
-
-for x,y in zip(score[0::2],score[1::2]):
-    x1 = int(x)
-    y1=int(y)
-    if x1>y1:
-        print(int(x),'>',int(y))
-    else:
-        print(int(x),'<',int(y))
-
-print(len(scoreboard))
 
 for s1,s2 in zip(scoreboard[0::2],scoreboard[1::2]):
     
       
-    print(s1.name,s1.score,sep=':') 
-    print(s2.name,s2.score,sep=':') 
+    print('Away',s1.name,s1.score,sep=':') 
+    print('Home',s2.name,s2.score,sep=':') 
     print('Team at Home won: ',s2.score > s1.score)
+    print('######################')
     #print(sc.score>scr.score)
     
     
