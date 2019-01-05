22bf5e94cd171fe4fdc3710c5c22f7cde8b6b0bf
Mateusz Leśko <mateusz.lesko@op.pl>
2019-01-05 15:01:11 +0100

    Change mechanisms
    Now user don't have to wait, until last match of the day end.

--- NBAScoreboard.py
+++ NBAScoreboard.py
@@ -26,62 +26,71 @@ def getTodaysDate(x):
     string = str(y)+"-"+str(m)+"-"+str(d)
     return string
 
-if d == getTodaysDate(datetime.datetime.now()):
-    print('You have to wait, all of the matches do not end yet')
-else:
 
-    page = requests.get('https://sports.yahoo.com/nba/scoreboard/?confId=&schedState=2&dateRange='+d)
-    soup = BeautifulSoup(page.content,'html.parser')
+page = requests.get('https://sports.yahoo.com/nba/scoreboard/?confId=&schedState=2&dateRange='+d)
+soup = BeautifulSoup(page.content,'html.parser')
     
-    list(soup.children)
+list(soup.children)
     
-    [type(item) for item in list(soup.children)]
+[type(item) for item in list(soup.children)]
     
-    teams = []
-    score = []
-    scoreboard = []
+teams = []
+score = []
+scoreboard = []
     
     
-    string = soup.find_all('span',{'data-tst':'first-name'})
+string = soup.find_all('span',{'data-tst':'first-name'})
     
-    for s in string:
+for s in string:
         
-     val = s.get_text()
-     teams.append(val)
+ val = s.get_text()
+ teams.append(val)
     
-    string= soup.find_all('div',{'class':'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'})
-    #print(string[2])
-    for s in string:
-        val = s.get_text()
-        score.append(val)
+string= soup.find_all('div',{'class':'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'})
+for s in string:
+   val = s.get_text()
+   score.append(val)
+    
     
         
-    print(len(teams),len(score),sep=',')
-    for x in range (0,len(teams),1):
+print(len(teams),len(score),sep=',')
+
+
+if d == getTodaysDate(datetime.datetime.now()):
+    
+    '''
+    delete match where haven't got score, still to come
+    '''
+    
+    for x in range(0,2):
+        teams.pop(0)
+        
+
+for x in range (0,len(teams),1):
       
-      scoreboard.append(Scoreboard(teams[x],int(score[x]),None,None))
+   scoreboard.append(Scoreboard(teams[x],int(score[x]),None,None))
     
-    for s1,s2 in zip(scoreboard[0::2],scoreboard[1::2]):
+for s1,s2 in zip(scoreboard[0::2],scoreboard[1::2]):
         
-        if s1.score > s2.score:
-            s1.win = True
-            s1.lose = False
-            s2.win = False
-            s2.lose = True
-        else:
-            s1.win = False
-            s1.lose = True
-            s2.win = True
-            s2.lose = False
+  if s1.score > s2.score:
+     s1.win = True
+     s1.lose = False
+     s2.win = False
+     s2.lose = True
+  else:
+     s1.win = False
+     s1.lose = True
+     s2.win = True
+     s2.lose = False
             
-        print('Away',s1.name,s1.score,sep=':') 
-        print('Home',s2.name,s2.score,sep=':') 
-        if s1.win == True:
-            print(s1.name,' won')
-        else:
-            print(s2.name,' won')
+  print('Away',s1.name,s1.score,sep=':') 
+  print('Home',s2.name,s2.score,sep=':') 
+  if s1.win == True:
+     print(s1.name,' won')
+  else:
+     print(s2.name,' won')
         #print('Team at Home won: ',s2.score > s1.score)
-        print('######################')
+  print('######################')
         
     
     
