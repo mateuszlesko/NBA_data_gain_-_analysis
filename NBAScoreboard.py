3570eb2580a522c437b5905253895116ba55eaf0
Mateusz Leśko <mateusz.lesko@op.pl>
2019-01-26 13:49:25 +0100

    see upcoming matches

--- NBAScoreboard.py
+++ NBAScoreboard.py
@@ -1,133 +1,198 @@
-from bs4 import BeautifulSoup
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 22:47:16 2019
+
+@author: 4ever
+
+metoda na upcoming jesli w live nie ma puktow dla druzyny oznacza, ze mecz dopiero
+odbedzie sie
+jesli druzyna ma zamiast rekordu pkty mecz odbedzie sie dopiero
+"""
 import datetime
 import requests
+from bs4 import BeautifulSoup as bs
 
 
-class FinishedMatchesScoreboard(object):
-    def __init__(self,name,score,win,lose):
-        self.name = name
+class FinishedScoreboard(object):
+    def __init__(self,first_name,last_name,score,win,lose):
+        
+        self.first_name = first_name
+        self.last_name = last_name
         self.score = score
         self.win = win
         self.lose = lose
-    
+class UpcomingSchedule(object):
+    def __init__(self,first_name,last_name,record):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.record = record
         
-def askForDate(d):
-    return d
-
-def getTodaysDate(x):
-    x = datetime.datetime.now()
-    y = x.year
-    m = x.month
-    d = x.day
-    
-    if m <10:
-        m = '0'+str(m)
-    if d <10:
-        d = '0'+str(d)
-    string = str(y)+"-"+str(m)+"-"+str(d)
-    return string
 
 
-page = requests.get('https://sports.yahoo.com/nba/scoreboard/?confId=&schedState=2&dateRange='+d)
-soup = BeautifulSoup(page.content,'html.parser')
+def ScrapeWebsiteWhenFinished(page):
     
-list(soup.children)
+    soup = bs(page.content,'html.parser')
     
-[type(item) for item in list(soup.children)]
+    list(soup.children)
     
-teams = []
-score = []
-scoreboard = []
-links = []
+    [type(item) for item in list(soup.children)]
     
+    first_names = []
+    last_names =  []
+    score = []
+    links = []
+   
     
-string = soup.find_all('span',{'data-tst':'first-name'})
+    string = soup.find_all('span',{'data-tst':'first-name'})
     
-for s in string:
+    for s in string:
         
- val = s.get_text()
- teams.append(val)
+        val = s.get_text()
+        first_names.append(val)
     
-string= soup.find_all('div',{'class':'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'})
-for s in string:
-   val = s.get_text()
-   score.append(val)
+    string = soup.find_all('span',{'data-tst':'last-name'})
+    
+    for s in string:
+        val = s.get_text()
+        last_names.append(val)
+    
+    string= soup.find_all('div',{'class':'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'})
+    for s in string:
+        val = s.get_text()
+        score.append(val)
 
-string = soup.find_all('a',{'C(primary-text) C(primary-text):link C(primary-text):visited Td(n) gamecard-final'})
+    string = soup.find_all('a',{'C(primary-text) C(primary-text):link C(primary-text):visited Td(n) gamecard-final'})
 
-for s in string:
-    link = 'https://sports.yahoo.com'
-    links.append(link+str(s.get('href')))
+    for s in string:
+        link = 'https://sports.yahoo.com'
+        links.append(link+str(s.get('href')))
+
+    index1 = 0
+    index2 = 1
+    for team1,team2 in zip(first_names[0::2],first_names[1::2]):
+        print(team1,last_names[index1],score[index1],sep=' ')
+        print(team2,last_names[index2],score[index2],sep=' ')
+        print("###############################")
+        index1 += 2
+        index2 += 2
+   
+    
+def ScrapeWebsiteWhenUpcoming(page):
+    first_names = []
+    last_names =  []
+    records = []
+  
+    
+    soup = bs(page.content,'html.parser')
+    
+    divs = soup.findAll('span',{'data-tst':'last-name'})
+    for div in divs:
+        last_names.append(div.text)
+    
+    divs = soup.findAll('span',{'data-tst':'first-name'})
+    for div in divs:
+        first_names.append(div.text)
+    #print(dates)
+    
+    divs = soup.findAll('div',{'class':'Whs(nw) D(tbc) Va(m) Fw(b) Fz(14px)'})
+    for div in divs:
+        records.append(div.text)
+    
+   
+     
+    index1 = 0
+    index2 = 1
+    
+    for team1,team2 in zip(first_names[0::2],first_names[1::2]):
         
-print(len(teams),len(score),sep=',')
-
-d = askForDate(input('date format (yyyy-mm-dd): '))
-if d == getTodaysDate(datetime.datetime.now()):
+        print(team1,last_names[index1],records[index1],sep=' ')
+        print(team2,last_names[index2],records[index2],sep=' ')
+        print("###############################")
+        index1 += 2
+        index2 += 2
+     
+    print('fn',len(first_names),'ln',len(last_names),'rec',len(records),sep=':')
+    return(first_names,last_names,records)
     
+def ScrapeWebsiteWhenLive(page):
+    score = []
+    first_names = []
+    last_names =  []
+    dates = []
+    
+    soup = bs(page.content,'html.parser')
+    
+    info = soup.find_all('div',{'class':'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'})
+    for inf in info:
+        #print(inf.text)
+        score.append(inf.text)
+        
+    first_n = soup.find_all('span',{'data-tst':'first-name'})
+    for fn in first_n:
+        first_names.append(fn.text)
+    
+    last_n = soup.find_all('span',{'data-tst':'last-name'})
+    for fn in last_n:
+        last_names.append(fn.text)
+    
+    divs = soup.findAll('div',{'class':'Ta(end) Cl(b) Fw(b) '})
+    for div in divs:
+        dates.append(div.text)
+    
+    x1=0
+    x2=1
     '''
-    delete match which haven't got score, still to come
+    for sc1,sc2 in zip(score[0:2],score[1:2]):
+        print(last_names[x1],sc1,dates[x1],'\n',sep=':')
+        print(last_names[x2],sc2,dates[x1],'\n',sep=':')
+        print('###########')
     '''
-    print('sometimes result can be mixed, check if matches ended in recent day')
-    
-    #for x in range(0,2):
-       # teams.pop(0)
-       # score.pop(0)
+    for ln1,ln2 in zip(last_names[0:2],last_names[1:2]):
+        print(ln1,score[x1],dates[x1],'\n',sep=':')
+        print(ln2,score[x2],dates[x1],'\n',sep=':')
+        print('###########')
+        x1+=1
+        x2+=1
         
-
-for x in range (0,len(teams),1):
-      
-   scoreboard.append(FinishedMatchesScoreboard(teams[x],int(score[x]),None,None))
-
-index = 0     
-for s1,s2 in zip(scoreboard[0::2],scoreboard[1::2]):
-       
-  if s1.score > s2.score:
-     s1.win = True
-     s1.lose = False
-     s2.win = False
-     s2.lose = True
-  else:
-     s1.win = False
-     s1.lose = True
-     s2.win = True
-     s2.lose = False
-            
-  print('Away',s1.name,s1.score,sep=':') 
-  print('Home',s2.name,s2.score,sep=':') 
-  print('details:',links[index])
-  if s1.win == True:
-     print(s1.name,' won')
-  else:
-     print(s2.name,' won')
-        #print('Team at Home won: ',s2.score > s1.score)
-  print('######################')
-  index+=1 
-
-#date = str(datetime.datetime.now())
-def WannaSave(x):
-    save = False
-    if(x=='Y' or x=='y'):
-        save = True
-    return save
-def CreateFile():
+        print(x1,x2)
+    '''    
+    print(score)    
+    print(first_names)
+    print(last_names)
+    print(dates)
+    '''
     
-    fname = '{date}.txt'.format(date=d)
     
-    f = open(fname,'w+')
-    string = ''
-    for s1,s2 in zip(scoreboard[0::2],scoreboard[1::2]):
-       string ='{team} : {score} \n'.format(team=s1.name,score=s1.score)
-       f.write(string)
-       string ='{team} : {score} \n'.format(team=s2.name,score=s2.score)
-       f.write(string)
-       if(s1.score>s2.score):
-           string="{team} won \n".format(team=s1.name)
-       else:
-           string="{team} won \n".format(team=s2.name)
+def main():
+    # my code here
+    d = input("Date format (yyyy-mm-dd): ")
+    page = requests.get('https://sports.yahoo.com/nba/scoreboard/?confId=&schedState=2&dateRange='+d)
+    soup = bs(page.content,'html.parser')
+
+    list(soup.children)
         
-       f.write(string)
-       f.write("############################### \n")
-      
-       
-if(WannaSave(input("If you want save results put 'Y' or 'y'"))==True):
-    CreateFile()
+    
+    [type(item) for item in list(soup.children)]
+    
+    #print(soup.title.string)
+    #div = soup.body.div.div
+    
+    #print(div.contents[0])
+    
+    divs = soup.body.div.div
+    
+    #print(divs.findAll('h3'))
+    h3s = divs.findAll('h3')
+    for h3 in h3s:
+        if h3.text=='Live':
+            print('live')
+            ScrapeWebsiteWhenLive(page)
+        if h3.text == 'Upcoming':
+            print('Upcoming')
+            ScrapeWebsiteWhenUpcoming(page)
+        if h3.text == 'Finished':
+            print('Finished')
+            ScrapeWebsiteWhenFinished(page)
+        
+if __name__ == "__main__":
+   main()
