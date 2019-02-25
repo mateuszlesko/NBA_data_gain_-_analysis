a0c9f35e81ac97bf37dd8e900d139ef0e8f8a985
Mateusz Leśko <mateusz.lesko@op.pl>
2019-02-25 15:42:17 +0100

    version Linux
    had to change input function --> raw_input for fedora 29 OS

--- conditions.py
+++ conditions.py
@@ -3,24 +3,24 @@
 
 @author: 4ever
 """
-import urllib.request
+import urllib3
 import requests
 class Condition():
-    
+
     def internet_on(self,url):
         try:
-            wp = urllib.request.urlopen(url)
-            pw = wp.read()
-            return True
-        except  urllib.error as err:
+            http = urllib3.PoolManager()
+            r = http.request('GET',url)
+            if r.status == 200:
+                return True
+        except  urllib3.exceptions.HTTPError as err:
             return False
-    
+
     def requestPage(self,url):
          page = requests.get(url)
-         
+
          if page.status_code == 200:
-         
+
              return page
-         
+
          return 0
-         
--- main.py
+++ main.py
@@ -11,49 +11,50 @@ from past import PastMatchup
 from live import LiveMatchup
 
 class ScheduleNBA():
-    
+
     def main(self):
-       
+
         condition = Condition()
         url = "https://sports.yahoo.com/nba/scoreboard/"
-        
+
         if condition.internet_on(url)==True:
             url = 'https://sports.yahoo.com/nba/scoreboard/?confId=&schedState=2&dateRange='
-            date = input('format(yyyy-mm-dd): ')
-            url+=date
-            
-            page = condition.requestPage(url)
+            date = raw_input('format(yyyy.mm.dd): ')
+            date = date.replace('.','-')
+            url2=url+date
+
+            page = condition.requestPage(url2)
            # soup = bs(page.content,'html.parser')
             if page !=0:
                 soup = bs(page.content,'html.parser')
                 sources = [item for item in soup.find_all('a',{'class':'gamecard-pregame'})]
-                
+
                 if len(sources)>0:
-                    
+
                     upcoming = UpcomingSchedule()
                     print('\n')
                     print('Upcoming matches: ',len(sources))
                     print(upcoming.scrapeFuture(soup,sources))
-                
-                sources = [item for item in soup.find_all('a',{'class':'gamecard-final'})]    
-                
+
+                sources = [item for item in soup.find_all('a',{'class':'gamecard-final'})]
+
                 if len(sources)>0:
-                    
+
                     final = PastMatchup()
                     print('\n')
                     print('Final results of matches')
                     print(final.ScrapePast(soup,sources))
-                
-                sources = [item for item in soup.find_all('a',{'class':'gamecard-in_progress'})]     
+
+                sources = [item for item in soup.find_all('a',{'class':'gamecard-in_progress'})]
                 if len(sources)>0:
                     print('\n')
                     print('Live results of matches')
                     live = LiveMatchup()
                     print(live.ScrapeLive(soup,sources))
-                    
+
         else:
             return "No connection to net"
         return 0
 if __name__ == '__main__':
     sn = ScheduleNBA()
-    sn.main()        
+    sn.main()
